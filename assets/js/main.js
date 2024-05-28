99fe97f6ce6306e83adb2e3e78600c9f1d5de822
Franco Morales Castro <franco.morales@outlook.com>
2024-05-27 20:41:35 -0400

    feat: main.js

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+// Crear la lista que gestionara las tareas
+const taskList = [];
+
+// Crear clase que represente a la tarea
+class Task {
+  constructor(name, description) {
+    
+  }
+  toggleCompletion() {}
+}
